a593028d4c631dc39dbe86757f5b4b29a34ab092
ArshiaMehtaab19 <arshiamehtaab@gmail.com>
2025-05-27 17:36:56 +0530

    Update User Engagement Index.sql

--- Module 2/User Engagement Index.sql
+++ Module 2/User Engagement Index.sql
@@ -1,1 +1,8 @@
+SELECT U.user_id, U.name,
+       COUNT(DISTINCT R.event_id) AS attended_events,
+       COUNT(DISTINCT F.event_id) AS feedbacks_given
+FROM Users U
+LEFT JOIN Registrations R ON U.user_id = R.user_id
+LEFT JOIN Feedback F ON U.user_id = F.user_id
+GROUP BY U.user_id, U.name;
 
