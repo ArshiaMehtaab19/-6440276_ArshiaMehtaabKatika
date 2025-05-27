ab260ffd4db4e9da41105f967437b99566e54cc4
ArshiaMehtaab19 <arshiamehtaab@gmail.com>
2025-05-27 17:28:45 +0530

    Update Feedback Gap.sql

--- Module 2/Feedback Gap.sql
+++ Module 2/Feedback Gap.sql
@@ -1,1 +1,7 @@
+SELECT E.event_id, E.name
+FROM Events E
+JOIN Registrations R ON E.event_id = R.event_id
+LEFT JOIN Feedback F ON R.event_id = F.event_id
+GROUP BY E.event_id, E.name
+HAVING COUNT(F.feedback_id) = 0;
 
