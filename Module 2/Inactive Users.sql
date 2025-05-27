8e0fae86040e06b1c49b14304a6ef33ca0a56fb6
ArshiaMehtaab19 <arshiamehtaab@gmail.com>
2025-05-27 17:20:26 +0530

    Update Inactive Users.sql

--- Module 2/Inactive Users.sql
+++ Module 2/Inactive Users.sql
@@ -1,1 +1,6 @@
+SELECT U.user_id, U.name
+FROM Users U
+LEFT JOIN Registrations R ON U.user_id = R.user_id
+GROUP BY U.user_id, U.name
+HAVING MAX(R.registration_date) IS NULL OR MAX(R.registration_date) < CURRENT_DATE - INTERVAL 90 DAY;
 
