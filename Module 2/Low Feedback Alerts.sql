2f478d9ce1bcefcf83f6fc78849fb2e5088e4856
ArshiaMehtaab19 <arshiamehtaab@gmail.com>
2025-05-27 17:25:46 +0530

    Update Low Feedback Alerts.sql

--- Module 2/Low Feedback Alerts.sql
+++ Module 2/Low Feedback Alerts.sql
@@ -1,1 +1,6 @@
+SELECT F.user_id, U.name, F.rating, F.comment, E.name AS event_name
+FROM Feedback F
+JOIN Users U ON F.user_id = U.user_id
+JOIN Events E ON F.event_id = E.event_id
+WHERE F.rating < 3;
 
