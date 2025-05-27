9d92cf364b8fb3490b48fa040c372bd9ca3c1504
ArshiaMehtaab19 <arshiamehtaab@gmail.com>
2025-05-27 17:23:47 +0530

    Update Event Resource Summary.sql

--- Module 2/Event Resource Summary.sql
+++ Module 2/Event Resource Summary.sql
@@ -1,1 +1,7 @@
+SELECT event_id,
+       SUM(CASE WHEN type = 'PDF' THEN 1 ELSE 0 END) AS pdfs,
+       SUM(CASE WHEN type = 'image' THEN 1 ELSE 0 END) AS images,
+       SUM(CASE WHEN type = 'link' THEN 1 ELSE 0 END) AS links
+FROM Resources
+GROUP BY event_id;
 
