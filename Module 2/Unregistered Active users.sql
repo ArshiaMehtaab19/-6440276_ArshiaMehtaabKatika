68e0cb909524055c86de8c71ff2461e1eb1be4f2
ArshiaMehtaab19 <arshiamehtaab@gmail.com>
2025-05-27 17:33:50 +0530

    Update Unregistered Active users.sql

--- Module 2/Unregistered Active users.sql
+++ Module 2/Unregistered Active users.sql
@@ -1,1 +1,7 @@
+SELECT U.user_id, U.name
+FROM Users U
+LEFT JOIN Registrations R ON U.user_id = R.user_id
+WHERE U.created_at >= CURRENT_DATE - INTERVAL 30 DAY
+GROUP BY U.user_id, U.name
+HAVING COUNT(R.event_id) = 0;
 
