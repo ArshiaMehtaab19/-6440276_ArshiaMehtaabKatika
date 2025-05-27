aee8997a339b2a39b7224efed00747919295cfd2
ArshiaMehtaab19 <arshiamehtaab@gmail.com>
2025-05-27 17:27:02 +0530

    Update Organizer Event Summary.sql

--- Module 2/Organizer Event Summary.sql
+++ Module 2/Organizer Event Summary.sql
@@ -1,1 +1,8 @@
+SELECT organizer_id,
+       COUNT(*) AS total_events,
+       SUM(CASE WHEN status = 'upcoming' THEN 1 ELSE 0 END) AS upcoming,
+       SUM(CASE WHEN status = 'completed' THEN 1 ELSE 0 END) AS completed,
+       SUM(CASE WHEN status = 'cancelled' THEN 1 ELSE 0 END) AS cancelled
+FROM Events
+GROUP BY organizer_id;
 
