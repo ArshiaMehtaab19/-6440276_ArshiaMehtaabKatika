a796342363f91169edb4798752b52c3e0dfdb18b
ArshiaMehtaab19 <arshiamehtaab@gmail.com>
2025-05-27 17:26:24 +0530

    Update Sessions per Upcoming Event.sql

--- Module 2/Sessions per Upcoming Event.sql
+++ Module 2/Sessions per Upcoming Event.sql
@@ -1,1 +1,6 @@
+SELECT E.event_id, E.name, COUNT(S.session_id) AS session_count
+FROM Events E
+LEFT JOIN Sessions S ON E.event_id = S.event_id
+WHERE E.start_date > CURRENT_DATE
+GROUP BY E.event_id, E.name;
 
