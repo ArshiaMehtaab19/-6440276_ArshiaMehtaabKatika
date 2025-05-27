50235204e8302954803dc93847198149cce6939f
ArshiaMehtaab19 <arshiamehtaab@gmail.com>
2025-05-27 17:32:45 +0530

    Update Event Session Time Conflict.sql

--- Module 2/Event Session Time Conflict.sql
+++ Module 2/Event Session Time Conflict.sql
@@ -1,1 +1,7 @@
+SELECT A.session_id AS session1, B.session_id AS session2, A.event_id
+FROM Sessions A
+JOIN Sessions B ON A.event_id = B.event_id
+WHERE A.session_id < B.session_id
+  AND A.start_time < B.end_time
+  AND B.start_time < A.end_time;
 
